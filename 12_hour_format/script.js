7c64bc9c370eb15125d3744ff2d4986aafaf0ba4
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-07-17 17:37:28 +0530

    Update script.js

--- 12_hour_format/script.js
+++ 12_hour_format/script.js
@@ -8,11 +8,11 @@ const clock = () => {
         let min = new Date().getMinutes();
         let sec = new Date().getSeconds();
         let am = 'AM';
-        if(hr > 12){
-            hr = hr - 12;
-        }
         if(hr >= 12){
             am='PM';
+        }
+        if(hr > 12){
+            hr = hr - 12;
         }
         hr = (hr<10) ? '0'+hr : hr;
         min = (min<10) ? '0'+min : min;
